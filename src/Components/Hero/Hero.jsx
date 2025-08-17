68b66ae448b886a16b7c702cc28cd7e1e7d5d2ee
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-17 20:06:22 +0530

    responsive nav and header with mobile menu animation added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,12 +4,12 @@ import Button from '../Button/Button'
 const Hero = () => {
   return (
     <section>
-      <div className=' bg-amber-300 min-h-screen max-w-[1400px] mx-auto px-10 flex items-center py-25'>
+      <div className=' bg-amber-300 min-h-screen max-w-[1400px] mx-auto px-10 flex md:flex-row flex-col items-center md:pt-25 pt-35'>
         {/* Hero Content */}
         <div className=' flex-1'>
           <span className=' bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'>Export Best Quality</span>
-          <h1 className='text-7xl/20 font-bold mt-4'>Tasty Orangic <span className=" text-orange-500">Fruits</span> & <span className=" text-orange-500">Veggies</span><br/> in your City</h1>
-          <p className=' text-zinc-600 text-lg max-w-[530px] mb-10 mt-5'>
+          <h1 className='md:text-7xl/20 text-5xl/14 font-bold mt-4'>Tasty Orangic <span className=" text-orange-500">Fruits</span> & <span className=" text-orange-500">Veggies</span><br/> in your City</h1>
+          <p className=' text-zinc-600 md:text-lg text-md max-w-[530px] mb-10 mt-5'>
             Bred for a high content of beneficial substances. Our products are all fresh and healthy.
           </p>
           <Button content={"Shop Now"}/>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import { GoHeartFill } from "react-icons/go";
 import { HiShoppingBag } from "react-icons/hi2";
 import { IoSearch } from "react-icons/io5";
+import { TbMenu2 } from "react-icons/tb";
+import { TbMenu3 } from "react-icons/tb";
+
 
 function Navbar() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMenu =  () => {
+    setMobileMenu(!mobileMenu);
+  }
   return (
 
     <header className=" bg-white fixed top-0 right-0 left-0">
-      <nav className="  max-w-[1400px] mx-auto px-10 h-[14vh] flex justify-between items-center">
+      <nav className="  max-w-[1400px] mx-auto px-10 md:h-[14vh] h-[12vh] flex justify-between items-center">
 
         <a className=" text-3xl font-bold">Gr<span className=" text-orange-500 uppercase">O</span>cify</a>
 
-        <ul className="flex items-center gap-x-15">
+        <ul className="md:flex items-center gap-x-15 hidden">
 
           {/* Nav Menu */}
           <li>
@@ -29,9 +38,9 @@ function Navbar() {
         </ul>
 
         {/* Nav Actions */}
-        <div className=" flex items-center gap-x-5" >
-          <div className=" flex items-center border border-slate-500 rounded-full p-1">
-            <input type="text" name="text" id="text" placeholder="Search..."  className=" outline-0 text-sm px-3"/>
+        <div className=" flex items-center gap-x-5 ml-25" >
+          <div className=" md:flex items-center border border-slate-500 rounded-full p-1 hidden">
+            <input type="text" name="text" id="text" placeholder="Search..." className=" outline-0 text-sm px-3" />
             <button className=" bg-gradient-to-b from-orange-400 to-orange-600 text-white text-2xl hover:cursor-pointer flex justify-center items-center w-10 h-10 rounded-full">
               <IoSearch />
             </button>
@@ -42,8 +51,39 @@ function Navbar() {
           <a className=" text-3xl text-zinc-800 hover:cursor-pointer hover:text-orange-500">
             <HiShoppingBag />
           </a>
+
+          <a onClick={toggleMenu} href="#" className=" text-zinc-800 text-3xl md:hidden">
+          {
+            mobileMenu? <TbMenu3 /> : <TbMenu2 />
+          }
+            
+          </a>
         </div>
 
+        {/* Hamburger Menu */}
+         <ul className={`flex flex-col mt-3 gap-y-15 bg-orange-500/15 backdrop-blur-lg p-10 items-center gap-x-15 mg:hidden absolute top-30 -left-full transform -translate-x-1/2 rounded-3xl transition-all duration-500 ${mobileMenu? "left-1/2": ""}`}>
+          {/* Nav Mobile Menu */}
+          <li>
+            <a href="#" className=" tracking-wider font-semibold text-white">Home</a>
+          </li>
+          <li>
+            <a href="#" className=" tracking-wider font-semibold text-zinc-800 hover:text-white">About</a>
+          </li>
+          <li>
+            <a href="#" className=" tracking-wider font-semibold text-zinc-800 hover:text-white">Process</a>
+          </li>
+          <li>
+            <a href="#" className=" tracking-wider font-semibold text-zinc-800 hover:text-white">Contact Us</a>
+          </li>
+          <li className="flex items-center border border-slate-500 rounded-full p-1 md:hidden">
+            <input type="text" name="text" id="text" placeholder="Search..." className=" outline-0 text-sm px-3" />
+            <button className=" bg-gradient-to-b from-orange-400 to-orange-600 text-white text-2xl hover:cursor-pointer flex justify-center items-center w-10 h-10 rounded-full">
+              <IoSearch />
+            </button>
+          </li>
+        </ul>
+        
+  
       </nav>
     </header >
   )
